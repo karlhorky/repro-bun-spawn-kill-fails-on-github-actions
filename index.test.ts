38da1d436d835b97014ef40ec2d6f339c2076cb5
Karl Horky <karl.horky@gmail.com>
2024-06-15 17:47:22 +0200

    Add snapshot and sleep

--- index.test.ts
+++ index.test.ts
@@ -1,12 +1,16 @@
-import { spawn } from 'bun';
+import { sleep, spawn } from 'bun';
 import { expect, test } from 'bun:test';
 
 test('Spawn and kill two child processes', async () => {
   // const childProcess1 = spawn(['python3', '-m', 'http.server', '8000']);
-  const childProcess1 = spawn(['yarn', 'run', 'dev']);
+  const childProcess1 = spawn(['yarn', 'run', 'dev'], {
+    stderr: 'pipe',
+  });
   console.log('childProcess1 started with PID', childProcess1.pid);
 
   const stdOutPromise = new Response(childProcess1.stdout).text();
+
+  await sleep(2000);
 
   childProcess1.kill();
   const childProcess1ExitCode = await childProcess1.exited;
@@ -14,7 +18,9 @@ test('Spawn and kill two child processes', async () => {
 
   expect(await stdOutPromise).toMatchSnapshot();
 
-  const childProcess2 = spawn(['yarn', 'run', 'dev']);
+  const childProcess2 = spawn(['yarn', 'run', 'dev'], {
+    stderr: 'pipe',
+  });
   console.log('childProcess2 started with PID', childProcess2.pid);
   childProcess2.kill();
   const childProcess2ExitCode = await childProcess2.exited;
