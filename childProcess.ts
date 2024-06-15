7c975ce6d6e5af3c79c1355353d63223f8a9ec43
Karl Horky <karl.horky@gmail.com>
2024-06-15 17:33:46 +0200

    Add express

--- childProcess.ts
+++ childProcess.ts
@@ -1,7 +1,12 @@
-import { serve } from 'bun';
+import express, { type Request, type Response } from 'express';
 
-serve({
-  fetch(request: Request) {
-    return new Response('Hello, World!');
-  },
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
